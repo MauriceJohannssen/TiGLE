09e00848d0317a387fb7b1cbb29dc00bd01eaba2
Maurice Johannssen <60900273+MauriceJohannssen@users.noreply.github.com>
2021-09-09 14:29:29 +0200

    Setup basic project setup with VBO, VAO, and EBO

--- TGLE/TGLE.cpp
+++ TGLE/TGLE.cpp
@@ -24,10 +24,19 @@ int main()
 	glViewport(0, 0, 1600, 900);
 
 	float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+		 0.5f,  0.5f, 0.0f,  // top right
+		 0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
 	};
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
 
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
@@ -36,6 +45,11 @@ int main()
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), reinterpret_cast<void*>(0));
 	glEnableVertexAttribArray(0);
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	//Vertex shader
 	const char* vertexShaderSource = "#version 330 core\n"
@@ -71,12 +85,12 @@ int main()
 
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
-	
+
 	while (window.isOpen())
 	{
 		HandleInput(&window);
 
-		//Draw
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		window.display();
 
